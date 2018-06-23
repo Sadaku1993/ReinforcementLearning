2a38179983a4f126bf774385005f93bc9f17c894
Sadaku1993 <36195395+Sadaku1993@users.noreply.github.com>
2018-06-24 06:08:05 +0900

    update

---
+++ DeepQLearning/dqn.py
@@ -0,0 +1,124 @@
+import numpy as np
+import tensorflow as tf
+import gym
+
+np.random.seed(1)
+tf.set_random_seed(1)
+
+class DeepQNetwork:
+    def __init__(
+        self,
+        n_actions,
+        n_features,
+        learning_rate = 0.01,
+        reward_decay = 0.9,
+        e_greedy = 0.9,
+        replace_target_iter = 300,
+        memory_size=500,
+        batch_size = 32,
+        output_batch=False
+    ):
+        self.n_actions = n_actions
+        self.n_features = n_features
+        self.lr = learning_rate
+        self.gamma = reward_decay
+        self.epsilon_max = e_greedy
+        self.replace_target_iter = replace_target_iter
+        self.memory_size = memory_size
+        self.batch_size = batch_size
+        self.epsilon = 0
+
+        self.learn_step_counter = 0
+
+        # initialize zero memory [s, a, r, s_]
+        self.memory = np.zeros((self.memory_size, n_features*2+2))
+
+        # target_netのVariableをbuild_netのVariableで更新
+        self._build_net()
+        t_params = tf.get_collection('target_net_params')
+        e_params = tf.get_collection('eval_net_params')
+        print("t_params", t_params)
+        print("e_params", e_params)
+        self.replace_target_op = [tf.assign(t, e) for t, e, in zip(t_params, e_params)]
+
+    def _build_net(self):
+        # ------------------ build evaluate_net ------------------
+        self.s = tf.placeholder(tf.float32, [None, self.n_features], name='s')  # input
+        self.q_target = tf.placeholder(tf.float32, [None, self.n_actions], name='Q_target')  # for calculating loss
+        with tf.variable_scope('eval_net'):
+            # c_names(collections_names) are the collections to store variables
+            c_names, n_l1, w_initializer, b_initializer = \
+                ['eval_net_params', tf.GraphKeys.GLOBAL_VARIABLES], 10, \
+                tf.random_normal_initializer(0., 0.3), tf.constant_initializer(0.1)  # config of layers
+
+            with tf.variable_scope('l1'):
+                w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
+                b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
+                l1 = tf.nn.relu(tf.matmul(self.s, w1) + b1)
+                print("Print name of Variables")
+                print(w1.name, b1.name)
+
+            with tf.variable_scope('l2'):
+                w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
+                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
+                self.q_eval = tf.matmul(l1, w2) + b2
+                print("Print name of Variables")
+                print(w2.name, b2.name)
+
+        with tf.variable_scope('loss'):
+            self.loss = tf.reduce_mean(tf.squared_difference(self.q_target, self.q_eval))
+        with tf.variable_scope('train'):
+            self._train_op = tf.train.RMSPropOptimizer(self.lr).minimize(self.loss)
+
+        # ------------------ build target_net ------------------
+        self.s_ = tf.placeholder(tf.float32, [None, self.n_features], name='s_')    # input
+        with tf.variable_scope('target_net'):
+            # c_names(collections_names) are the collections to store variables
+            c_names = ['target_net_params', tf.GraphKeys.GLOBAL_VARIABLES]
+
+            # first layer. collections is used later when assign to target net
+            with tf.variable_scope('l1'):
+                w1 = tf.get_variable('w1', [self.n_features, n_l1], initializer=w_initializer, collections=c_names)
+                b1 = tf.get_variable('b1', [1, n_l1], initializer=b_initializer, collections=c_names)
+                l1 = tf.nn.relu(tf.matmul(self.s_, w1) + b1)
+                print("Print name of Variables")
+                print(w1.name, b1.name)
+
+            # second layer. collections is used later when assign to target net
+            with tf.variable_scope('l2'):
+                w2 = tf.get_variable('w2', [n_l1, self.n_actions], initializer=w_initializer, collections=c_names)
+                b2 = tf.get_variable('b2', [1, self.n_actions], initializer=b_initializer, collections=c_names)
+                self.q_next = tf.matmul(l1, w2) + b2
+                print("Print name of Variables")
+                print(w2.name, b2.name)
+
+    def store_transition(self, s, a, r, s_):
+        if not hasattr(self, 'memory_counter'):
+            self.memory_counter = 0
+
+        transition = np.hstack((s, [a, r], s_))
+
+        # replace the old memory with new memory
+        index = self.memory_counter % self.memory_size
+        self.memory[index, :] = transition
+
+        self.memory_counter += 1
+
+
+env = gym.make('CartPole-v0')
+env.seed(1)
+
+N_F = env.observation_space.shape[0]
+N_A = env.action_space.n
+
+sess = tf.Session()
+dqn = DeepQNetwork(n_actions=N_A, 
+                   n_features=N_F,
+                   learning_rate=0.01,
+                   reward_decay=0.9,
+                   e_greedy=0.9,
+                   replace_target_iter=200,
+                   memory_size=2000
+                   )
+
+
